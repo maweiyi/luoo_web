cb29e19e0891ebcca93c090099f31a69cc8960cd
maweiyi <ma823285807@gmail.com>
2017-02-26 20:01:00 +0800

    add README.md

--- public/js/index.js
+++ public/js/index.js
@@ -4,5 +4,13 @@ new Vue({
     el: '#index',
     data: {
         name: 'KKKKKKS'
+    },
+    methods: {
+        click: function () {
+            console.log("Click Me!");
+        },
+        clickOther: function () {
+            console.log("LLLLLLLLLLL");
+        }
     }
 });
